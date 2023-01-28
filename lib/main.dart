f95c7ee3ba0edf471c4bfb4b735e6ab5dca49bbc
muhammadmusharaf35@gmail.com <muhammadmusharaf35@gmail.com>
2023-01-28 17:45:01 +0500

    Splash screen added using native_splash_screen

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,13 @@ import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 
 void main() {
+   WidgetsFlutterBinding.ensureInitialized();
+  // FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  
   const MyApp({super.key});
 
   // This widget is the root of your application.
--- lib/screens/login/login_controller.dart
+++ lib/screens/login/login_controller.dart
@@ -1,3 +1,10 @@
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:get/get.dart';
 
-class LoginController extends GetxController {}
+class LoginController extends GetxController {
+  void onInit() {
+    
+    super.onInit();
+    FlutterNativeSplash.remove();
+  }
+}
